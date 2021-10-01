0cf1e868c9241145e2a774b446b5fca327ed21e6
Gustavo Lima <gustavolima@ciandt.com>
2021-10-01 08:40:16 -0300

    Add jest testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from './App';
 import { store } from './app/store';
+import App from './App';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
